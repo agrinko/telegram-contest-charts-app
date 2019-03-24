28703584cb698e8191bcf4b3ff4fc0348d53a28c
Alexey Grinko <agrinko@whiz.ai>
2019-03-24 02:37:39 +0200

    X-axis (another bad implementation)

--- src/modules/components/Chart/Axis.js
+++ src/modules/components/Chart/Axis.js
@@ -43,24 +43,62 @@ export class Axis {
     let diff = hi -lo;
     let step = Math.max(MINIMAL_TIME_STEP / 24, diff / this.nSteps);
 
-    this.labels = [];
-    this.step = step;
+    this.nodes = [{
+      x: lo,
+      left: 0,
+      step
+    }, {
+      x: hi,
+      left: 1,
+      step
+    }];
 
-    for (let t = lo - step; t <= hi + step; t += step) {
-      this.labels.push(this._createLabel(t));
-    }
+    this.fillNodes(this.nodes[0], this.nodes[1], step);
 
-    console.log(this.labels);
+    // this.labels = [];
+    // this.step = step;
+    //
+    // for (let t = lo - step; t <= hi + step; t += step) {
+    //   this.labels.push(this._createLabel(t));
+    // }
+
+    console.log(this.nodes, step);
   }
 
-  _createLabel(t) {
-    let diff = this.bounds[1] - this.bounds[0];
+  fillNodes(n1, n2, step) {
+    let [lo, hi] = this.bounds;
+    let diff = hi -lo;
+
+    if (n2.x - n1.x > step * 0.75) {
+      let nNew = {
+        x: (n2.x + n1.x) / 2,
+        parentL: n1,
+        parentR: n2,
+        prev: n1,
+        next: n2,
+        step: step,
+        left: ((n2.x + n1.x) / 2 - this.bounds[0]) / diff,
+      };
+      n1.next = nNew;
+      n2.prev = nNew;
+      this.nodes.push(nNew);
+
+      this.fillNodes(n1, nNew, step);
+      this.fillNodes(nNew, n2, step);
+    }
+  }
+
+  _createLabel(node) {
+    let [lo, hi] = this.bounds;
+    let diff = hi -lo;
+    let step = Math.max(MINIMAL_TIME_STEP / 24, diff / this.nSteps);
+    let distanceToParent = node.parentL ? node.x - node.parentL.x : step;
 
     return {
-      t,
-      opacity: 1,
-      text: format(t), // TODO: optimize to only call format() if day is different; show hours when needed
-      left: (t - this.bounds[0]) / diff,
+      t: node.x,
+      opacity: Math.max(0, Math.min(1, 2 * distanceToParent / step - 1)),
+      text: format(node.x), // TODO: optimize to only call format() if day is different; show hours when needed
+      left: node.left,
       diff
     };
   }
@@ -68,44 +106,63 @@ export class Axis {
   _move(deltaLo, deltaHi) {
     let [lo, hi] = this.bounds;
     let diff = hi -lo;
-    let step = this.step;
+    let step = Math.max(MINIMAL_TIME_STEP / 24, diff / this.nSteps);
 
-    this.labels.forEach(l => {
-      l.left = (l.t - this.bounds[0]) / diff;
+    let leftNode, rightNode;
+
+    this.nodes.forEach(node => {
+      node.left = (node.x - lo) / diff;
+      if (!leftNode || leftNode.left > node.left)
+        leftNode = node;
+      if (!rightNode || rightNode.left < node.left)
+        rightNode = node;
     });
 
-    for (let t = this.labels[0].t - step; t >= lo - step; t -= step) {
-      this.labels.unshift(this._createLabel(t));
+    let actualStep = leftNode.step;
+
+    if (leftNode.x >= lo + actualStep) {
+      let ddd = rightNode.x - leftNode.x;
+      let newX = leftNode.x - ddd;
+      let newNode = {
+        x: newX,
+        left: (newX - lo) / diff,
+        step: actualStep
+      };
+      this.nodes.push(newNode);
+      leftNode.parentR = rightNode;
+      leftNode.parentL = newNode;
+      this.fillNodes(newNode, leftNode, actualStep);
     }
-
-    for (let t = this.labels[this.labels.length - 1].t + step; t <= hi + step; t += step) {
-      this.labels.push(this._createLabel(t));
-    }
-
-    let labelsVisible = this.labels.filter(l => l.opacity > 0.5);
-    let ll = labelsVisible.length - 3;
-
-console.log(`ll: ${ll}, nSteps: ${this.nSteps}`);
-      this.labels.forEach((l, i) => {
-        if (i%2 == 0)
-          l.opacity = 2 - ll / this.nSteps;
-      });
-
-
-    // let x1 = this.labels[1].t;
+    //
+    // actualStep = rightNode.step;
+    // if (rightNode.x <= lo - actualStep/2) {
+    //   let newX = rightNode.x + 2 * actualStep;
+    //   let newNode = {
+    //     x: newX,
+    //     left: (newX - lo) / diff,
+    //     prev: rightNode,
+    //     step: actualStep
+    //   };
+    //   this.nodes.push(newNode);
+    //   this.fillNodes(rightNode, newNode, actualStep);
+    // }
 
     this._renderLabels();
   }
 
   _renderLabels() {
-    let html = '';
-
-    this.labels.forEach(l => {
+    this.nodes.forEach(node => {
+      let l = this._createLabel(node);
       let tx = Math.max(0, Math.min(100, l.left * 100));
-      html += `<div class="label" style="left: ${l.left * 100}%; opacity: ${l.opacity}; transform: translateX(${-tx}%)">${l.text}</div>`;
+      let el = DOM.elementFromString(`<div class="label" style="left: ${l.left * 100}%; opacity: ${l.opacity}; transform: translateX(${-tx}%)">${l.text}</div>`);
+
+      if (node.el)
+        this.el.removeChild(node.el);
+
+      node.el = el;
+      this.el.appendChild(el);
     });
 
-    this.el.innerHTML = html;
   }
 
   _getNumberOfSteps() {
